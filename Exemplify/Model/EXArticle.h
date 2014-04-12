998eee4c095507b356610a5c1ad587ce3570b5c8
Vassil Mladenov <VassilMladenov@wirelessprvnat-172-16-209-86.near.illinois.edu>
2014-04-11 23:51:56 -0500

    Added Article class

---
+++ Exemplify/Model/EXArticle.h
@@ -0,0 +1,13 @@
+//
+//  EXArticle.h
+//  Exemplify
+//
+//  Created by Vassil Mladenov on 4/11/14.
+//  Copyright (c) 2014 Confluence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EXArticle : NSObject
+
+@end
