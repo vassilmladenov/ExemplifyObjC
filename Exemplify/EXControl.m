0a9bbdd9ae92755c06ececddf95a9dde41ed39ef
David Prorok <Me@Davids-Mac.local>
2014-04-12 11:05:59 -0700

    attempt at parsing html

--- Exemplify/EXControl.m
+++ Exemplify/EXControl.m
@@ -25,19 +25,35 @@
 	NSURL *wikiURL = [[NSURL alloc] initWithScheme:@"http" host:@"en.wikipedia.org" path:[@"/wiki/" stringByAppendingString:query]];
 	// get block of URLs
 	NSData *wikiData = [NSData dataWithContentsOfURL:wikiURL];
-//	NSError *error = Nil;
-//	NSString *a = [NSString stringWithContentsOfURL:wikiURL encoding:NSUTF8StringEncoding error:&error];
-//	
-//	NSString *startString = @"<h2><span class=\"mw-headline\" id=\"References\">References</span></h2>";
-//	NSString *endString = @"<h2><span class=\"mw-headline\" id=\"External_links\">External links</span></h2>";
-//	
-//	NSRange r1 = [a rangeOfString:startString];
-//	NSRange r2 = [a rangeOfString:endString];
-//	NSRange rSub = NSMakeRange(r1.location + r1.length, r2.location - r1.location - r1.length);
-//	NSString *b = [a substringWithRange:rSub];
+	NSError *error = Nil;
+	NSString *a = [NSString stringWithContentsOfURL:wikiURL encoding:NSUTF8StringEncoding error:&error];
+	
+	NSString *startString = @"<ol class=\"references\">";
+	NSString *endString = @"<h2><span class=\"mw-headline\" id=\"External_links\">External links</span></h2>";
+	
+	NSRange r1 = [a rangeOfString:startString];
+	NSRange r2 = [a rangeOfString:endString];
+	NSRange rSub = NSMakeRange(r1.location + r1.length, r2.location - r1.location - r1.length);
+	NSString *b = [a substringWithRange:rSub];
+    
+    NSMutableArray *refs = [[NSMutableArray alloc]initWithArray:[b componentsSeparatedByString:@"</li>"]];
+    
+    for (int i = 0; i< [refs count]; i++){
+        NSString *startString = @"<li>";
+        //NSString *endString = @"<h2><span class=\"mw-headline\" id=\"External_links\">External links</span></h2>";
 
+        NSRange start = [refs[i] rangeOfString:startString];
+        if (start.location != NSNotFound)
+        {
+            NSString *b2 = [refs[i] substringFromIndex:start.location + start.length];
+            NSLog(@"%@",b2);
+            refs[i] = [[NSString alloc] initWithString:b2];
+        }
+
+        
+    }
 	// for every URL in the list
-//	NSLog(@"%@",b);
+	//NSLog(@"%@",refs);
 	// add to array
 
 	return sources;
