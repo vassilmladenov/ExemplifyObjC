4fd833544f22bc37f09989856b9a39ffe652437e
David Prorok <Me@Davids-Mac.local>
2014-04-11 22:34:40 -0700

    .

---
+++ Exemplify/EXArticle.h
@@ -0,0 +1,13 @@
+//
+//  EXArticle.h
+//  Exemplify
+//
+//  Created by David Prorok on 4/11/14.
+//  Copyright (c) 2014 Confluence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EXArticle : NSObject
+
+@end
---
+++ Exemplify/EXArticle.m
@@ -0,0 +1,13 @@
+//
+//  EXArticle.m
+//  Exemplify
+//
+//  Created by David Prorok on 4/11/14.
+//  Copyright (c) 2014 Confluence. All rights reserved.
+//
+
+#import "EXArticle.h"
+
+@implementation EXArticle
+
+@end
