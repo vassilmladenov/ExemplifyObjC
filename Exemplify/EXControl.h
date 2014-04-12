a75f54d40e22a0cc2037e80df24e430675ea2df0
Vassil Mladenov <VassilMladenov@wirelessprvnat-172-16-209-86.near.illinois.edu>
2014-04-12 00:25:07 -0500

    added control

---
+++ Exemplify/EXControl.h
@@ -0,0 +1,15 @@
+//
+//  EXControl.h
+//  Exemplify
+//
+//  Created by Vassil Mladenov on 4/12/14.
+//  Copyright (c) 2014 Confluence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EXControl : NSObject
+
+// added headers
+
+@end
---
+++ Exemplify/EXControl.m
@@ -0,0 +1,15 @@
+//
+//  EXControl.m
+//  Exemplify
+//
+//  Created by Vassil Mladenov on 4/12/14.
+//  Copyright (c) 2014 Confluence. All rights reserved.
+//
+
+#import "EXControl.h"
+
+@implementation EXControl
+
+// added .m
+
+@end
